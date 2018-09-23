275a6ad21493d7d48ea6224d00c4da2394ff83e0
rafaeldellaquila <rafadellaquila@gmail.com>
2018-09-23 16:02:03 -0300

    funcionalidaed de remover e animacao do botao X

--- projeto_live_marcobrunobr/js/components/form-collab.js
+++ projeto_live_marcobrunobr/js/components/form-collab.js
@@ -45,23 +45,25 @@ passo a passo:
 
 */
 /* $ usado para identificar  uma referencia no DOM */
-    /* 
-    const keyCode = event.keyCode
-    const keys = [13]
-    keys[13] = "Enter" 
-    */
+/* 
+const keyCode = event.keyCode
+const keys = [13]
+keys[13] = "Enter" 
+*/
 'use strict'
 
 const $formCollab = window.document.querySelector('.form-collab') // Const que seleciona pra mim o .form-collab
 const $label = $formCollab.querySelector('.label') // Seleciona o label dentro do form
-const $input = $formCollab.querySelector('.input')// seleciona o input dentro do form
+const $input = $formCollab.querySelector('.input') // seleciona o input dentro do form
 const $tags = $formCollab.querySelector('.tags') //seleciona a classe tags
 
 const focusAndBlur = () => $label.classList.toggle('-focus') // Arow function (Sempre anonimas) // toggle vai literalmente alternar  a classe -focus,
 const newTag = value => {
     const $tag = document.createElement('span')
-    const template = `<input class="tagInput" id="${value}" type="checkbox" checked><label style="margin-left: 5px;"class="tag" for="${value}">${value}</label>`
-      //Template string com os lugares onde eu qero que mude o value indicado por '${x}' 
+    const template =
+        `<input class="tagInput" id="${value}" type="checkbox" checked>
+    <label style="margin-left: 5px;"class="tag" for="${value}">${value}</label>`
+    //Template string com os lugares onde eu qero que mude o value indicado por '${x}' 
     $tag.innerHTML = template
 
     return $tag
@@ -72,14 +74,34 @@ $input.addEventListener('blur', focusAndBlur) // Add evento ao input quando ele 
 
 $formCollab.addEventListener('submit', event => event.preventDefault()) // Previne o Refresh no form quando pressiona enter
 
-$input.addEventListener('keyup', event => {  
+$input.addEventListener('keyup', event => {
 
-   const {keyCode} = event
-   const keys = { 13: "Enter" }
+    const {
+        keyCode
+    } = event
+    const keys = {
+        13: "Enter" //Revisar 
+    }
 
     if (keys[keyCode] === "Enter") { //quando uma tecla for pressionada, se foro enter entra nop bloco true
-      const {value} = $input //Guarda o .value do que foi digitado dentro do input após o keyup para ser substituido no template
-      $tags.prepend(newTag(value))
-      $input.value = ''
+        const {
+            value
+        } = $input //Guarda o .value do que foi digitado dentro do input após o keyup para ser substituido no template
+        $tags.prepend(newTag(value)) //Adiciona elemento na frente da tag html
+        $input.value = ''
+    }
+})
+
+$tags.addEventListener('click', event => {
+
+    const {
+        target
+    } = event
+
+    if (target.classList.contains('remove')) {
+        const $wrapTag = target.closest('.wrapTag')
+
+        $wrapTag.classList.add('-remove')
+        $wrapTag.addEventListener('transitionend', () => $wrapTag.remove())
     }
 })
