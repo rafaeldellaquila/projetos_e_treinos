edee8683d68a072e1024fad34a4e3c1bb7d16335
rafaeldellaquila <rafadellaquila@gmail.com>
2018-09-23 13:15:16 -0300

    refatoraçao criação da contante newTag pra guardar os valores de template e retornar a $tag

--- projeto_live_marcobrunobr/js/components/form-collab.js
+++ projeto_live_marcobrunobr/js/components/form-collab.js
@@ -58,6 +58,14 @@ const $input = $formCollab.querySelector('.input')// seleciona o input dentro do
 const $tags = $formCollab.querySelector('.tags') //seleciona a classe tags
 
 const focusAndBlur = () => $label.classList.toggle('-focus') // Arow function (Sempre anonimas) // toggle vai literalmente alternar  a classe -focus,
+const newTag = value => {
+    const $tag = document.createElement('span')
+    const template = `<input class="tagInput" id="${value}" type="checkbox" checked><label style="margin-left: 5px;"class="tag" for="${value}">${value}</label>`
+      //Template string com os lugares onde eu qero que mude o value indicado por '${x}' 
+    $tag.innerHTML = template
+
+    return $tag
+}
 
 $input.addEventListener('focus', focusAndBlur) // Add evento ao input quando ele for focado(Clicado)
 $input.addEventListener('blur', focusAndBlur) // Add evento ao input quando ele for Desfocado(Clique sair)
@@ -70,14 +78,8 @@ $input.addEventListener('keyup', event => {
    const keys = { 13: "Enter" }
 
     if (keys[keyCode] === "Enter") { //quando uma tecla for pressionada, se foro enter entra nop bloco true
-      
-      const $wrap = document.createElement('span')
       const {value} = $input //Guarda o .value do que foi digitado dentro do input após o keyup para ser substituido no template
-      const template = `<input class="tagInput" id="${value}" type="checkbox" checked><label class="tag" for="${value}">${value}</label>`
-      //Template string com os lugares onde eu qero que mude o value indicado por '${x}' 
-      
-      $wrap.innerHTML = template
-      $tags.prepend($wrap)
+      $tags.prepend(newTag(value))
       $input.value = ''
     }
 })
